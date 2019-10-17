72b9a4fea0f182ad7135371f6d185e6afb685416
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-10-17 09:57:02 +0100

    results.json is not necessarily present - account for this potential absence

--- program/mlperf-inference-vision/postprocess.py
+++ program/mlperf-inference-vision/postprocess.py
@@ -44,8 +44,11 @@ def ck_postprocess(i):
     mlperf_log_dict['detail'] = detail_file.readlines()
 
   # Read output.
-  with open(RESULTS_JSON, 'r') as results_file:
-    save_dict['results'] = json.load(results_file)
+  try:
+      with open(RESULTS_JSON, 'r') as results_file:
+        save_dict['results'] = json.load(results_file)
+  except:
+    pass
 
   # Check accuracy in accuracy mode.
   accuracy_mode = False
@@ -74,18 +77,20 @@ def ck_postprocess(i):
     with open(COCO_RESULTS_JSON, 'r') as coco_results_file:
       save_dict['results-coco'] = json.load(coco_results_file)
 
-  for scenario in [ 'SingleStream', 'MultiStream', 'Server', 'Offline' ]:
-    scenario_key = 'TestScenario.%s' % scenario
-    scenario = save_dict['results'].get(scenario_key, None)
-    if scenario: # FIXME: Assumes only a single scenario is valid.
-      save_dict['execution_time_s']  = scenario.get('took', 0.0)
-      save_dict['execution_time_ms'] = scenario.get('took', 0.0) * 1000
-      save_dict['percentiles'] = scenario.get('percentiles', {})
-      save_dict['qps'] = scenario.get('qps', 0)
-      if accuracy_mode:
-        ck.out('mAP=%.3f%% (from the results for %s)' % (scenario.get('mAP', 0.0) * 100.0, scenario_key))
+  for scenario_name in [ 'SingleStream', 'MultiStream', 'Server', 'Offline' ]:
+    scenario_key = 'TestScenario.%s' % scenario_name
+    if 'results' in save_dict:
+        scenario = save_dict['results'].get(scenario_key, None)
 
-  save_dict['execution_time'] = save_dict['execution_time_s']
+        if scenario: # FIXME: Assumes only a single scenario is valid.
+          save_dict['execution_time_s']  = scenario.get('took', 0.0)
+          save_dict['execution_time_ms'] = scenario.get('took', 0.0) * 1000
+          save_dict['percentiles'] = scenario.get('percentiles', {})
+          save_dict['qps'] = scenario.get('qps', 0)
+          if accuracy_mode:
+            ck.out('mAP=%.3f%% (from the results for %s)' % (scenario.get('mAP', 0.0) * 100.0, scenario_key))
+
+  # save_dict['execution_time'] = save_dict['execution_time_s']
   with open('tmp-ck-timer.json', 'w') as save_file:
     json.dump(save_dict, save_file, indent=2, sort_keys=True)
 
